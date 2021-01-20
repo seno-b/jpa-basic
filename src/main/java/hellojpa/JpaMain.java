e36e7d63e59c0d881d6ce53a85fa486c53ae9599
seno-b <bangshinho@gmail.com>
2021-01-20 13:02:29 +0900

    기본 생성

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,31 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+
+            Member findMember = em.find(Member.class, 2L);
+            findMember.setName("수정수정");
+
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        }finally{
+            em.close();
+            emf.close();
+        }
+    }
+}
