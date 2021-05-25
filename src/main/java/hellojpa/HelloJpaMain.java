0ef94d82f7211a5ee7d85143eec1a965dcd5b192
seno-b <bangshinho@gmail.com>
2021-05-26 01:20:34 +0900

    feature: section 5. 연관관계 매핑 기초  - 양방향 연관관계와 연관관계의 주인 1 - 기본

--- src/main/java/hellojpa/HelloJpaMain.java
+++ src/main/java/hellojpa/HelloJpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class HelloJpaMain {
 
@@ -34,10 +35,11 @@ public class HelloJpaMain {
             em.clear();
             Member findMember = em.find(Member.class, member.getId());
 
-            System.out.println(findMember.getTeam());
+            List<Member> members = findMember.getTeam().getMembers();
 
-            Team newTeam = em.find(Team.class, 9L);
-            findMember.setTeam(newTeam);
+            for(Member m : members){
+                System.out.println(m.getUsername());
+            }
 
             tx.commit();
         }catch (Exception e){
