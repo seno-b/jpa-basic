aa40200a728c6ade5982e9298774971cb37f9d54
seno-b <bangshinho@gmail.com>
2021-06-19 20:02:10 +0900

    [feature] : section 10. 객체지향 쿼리 언어1 - 기본 문법 - 소개 (3/3) - 기본 문법과 쿼리 API (1/2)

---
+++ src/main/java/jpql/JpqlMain.java
@@ -0,0 +1,27 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpqlMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jqpl");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();
+            emf.close();
+        }
+    }
+}
