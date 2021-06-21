b85eb6f15185e5e8644067836f1d9da3e2557e96
seno-b <bangshinho@gmail.com>
2021-06-21 23:27:01 +0900

    [feature] : section 10. 객체지향 쿼리 언어1 - 기본 문법 - 프로젝션 (SELECT)

--- src/main/java/jpql/JpqlMain.java
+++ src/main/java/jpql/JpqlMain.java
@@ -1,9 +1,13 @@
 package jpql;
 
+import jpql.DTO.MemberDTO;
+import jpql.domain.Member;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpqlMain {
 
@@ -14,6 +18,35 @@ public class JpqlMain {
         EntityTransaction tx = em.getTransaction();
         try {
             tx.begin();
+
+            Member m = new Member();
+            m.setUsername("Member1dd");
+            m.setAge(30);
+
+            Member m2 = new Member();
+            m2.setUsername("Member2");
+            m2.setAge(30);
+
+            em.persist(m);
+            em.persist(m2);
+
+            Member findMember = em.createQuery("select m from Member m where username = :username", Member.class)
+                    .setParameter("username", "Member1dd")
+                    .getSingleResult();
+
+            System.out.println("findMember = " + findMember.getId());
+
+            findMember.setAge(500);
+
+            em.flush();
+            em.clear();
+
+            List<MemberDTO> resultList = em.createQuery("select new jpql.DTO.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            for (MemberDTO o : resultList) {
+                System.out.println("o = " + o.getUsername());
+            }
 
             tx.commit();
         }catch (Exception e){
