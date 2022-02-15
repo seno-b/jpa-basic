8e629cb557e25ea4f790cdb7ea4f173fca004e6f
seno-b <bangshinho@gmail.com>
2022-02-16 00:17:26 +0900

    feature: 3.4 영속성 컨텍스트의 특징
    - 3.4.2 엔티티 등록

--- src/main/java/jpabook/start/JpaMain.java
+++ src/main/java/jpabook/start/JpaMain.java
@@ -18,20 +18,20 @@ public class JpaMain {
 
     Member member = new Member();
     member.setId(1l);
-    member.setName("어쩔티비");
-    member.setAge(555);
+    member.setName("신규생성1");
+    member.setAge(12);
+
+    Member member2 = new Member();
+    member.setId(2l);
+    member.setName("신규생성2");
+    member.setAge(22);
 
     em.persist(member);
-    em.flush();
-    em.clear();
+    em.persist(member2);
 
-    Member findMember1 = em.find(Member.class, 1l);
-    // 1차 캐시에서 바로 꺼내기 때문에 sql select query 한번만 실행
-    Member findMember2 = em.find(Member.class, 1l);
-
-    // 같은 트랜잭션 안에서 동일성 보장
-    System.out.println(findMember1 == findMember2);
-
+    // 엔티티 매니저는 트랜잭션을 커밋하기 직전까지 데이터베이스에 엔티티를 저장하지 않고 내부 쿼리 저장소에 INSERT QUERY를 차곡차곡 저장해둔다
+    // 그리고 트랜잭션을 커밋할 때 모아둔 쿼리를 데이터베이스에 보내는데 이것을 트랜잭션을 지원하는 쓰기지연(transactional write-behind) 라고 한다.
+    // 커밋하는 순간 insert query 보낸다.
     tx.commit();
     em.close();
     emf.close();
