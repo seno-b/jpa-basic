30a838455de94cfe6c54728c60a0b5648b2b8ee0
seno-b <bangshinho@gmail.com>
2022-02-16 01:03:37 +0900

    feature: 3.6 준영속
    - 3.6.2 영속성 컨텍스트 초기화: clear()

--- src/main/java/jpabook/start/JpaMain.java
+++ src/main/java/jpabook/start/JpaMain.java
@@ -23,9 +23,11 @@ public class JpaMain {
 
     // 영속 상태, 1차 캐시 저장, 쓰기지연 쿼리 생성
     em.persist(member);
+    em.flush();
+    em.clear();
 
-    // 비영속 상태, 1차 캐시 제거, 쓰기지연 쿼리 제거
-    em.detach(member);
+    // 비영속 상태, 더 이상 dirty checking 되지 않음.
+    member.setName("change name");
 
     tx.commit();
     em.close();
